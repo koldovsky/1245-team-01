56f77f69c3caa171b05315768687fd2333ecfe7b
Александр Д <palbma>
2024-08-23 11:11:50 +0300

    fixed slider

--- js/index/index.slider-albums.js
+++ js/index/index.slider-albums.js
@@ -18,8 +18,7 @@ document.body.addEventListener("htmx:afterSettle", (event) => {
       carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
     });
   carouselChildrens
-    .slice(-cardPerView)
-    .reverse()
+    .slice(0, cardPerView)
     .forEach((card) => {
       carousel.insertAdjacentHTML("beforeend", card.outerHTML);
     });
@@ -28,7 +27,7 @@ document.body.addEventListener("htmx:afterSettle", (event) => {
     btn.addEventListener("click", () => {
       carousel.scrollLeft +=
         btn.id === "left" ? -firstCardWidth : firstCardWidth;
-        infiniteScroll();
+      infiniteScroll();
     });
   });
 
@@ -39,34 +38,35 @@ document.body.addEventListener("htmx:afterSettle", (event) => {
     startScrollLeft = carousel.scrollLeft;
   };
 
-  const dragStop = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
+  const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
   };
 
   const dragging = (e) => {
     if (!isDragging) return;
-    carousel.scrollLeft = e.pageX;
     carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
   };
 
-  const infiniteScroll = () =>{
-    if(carousel.scrollLeft === 0){
+  const infiniteScroll = () => {
+    if (carousel.scrollLeft === 0) {
       carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-
-    }
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    } else if (Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.offsetWidth) {
       carousel.scrollLeft = carousel.offsetWidth;
     }
-    
     clearTimeout(timeoutId);
-    if(!wrapper.matched(":hover")) autoPlay();
-  } 
-  const autoPlay = () =>{
-    if(window.innerWidth < 800) return;
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 150);
-  }
-  autoPlay();
+    if (!wrapper.matches(":hover")) autoPlay();
+  };
+
+  const autoPlay = () => {
+    if (window.innerWidth < 800) return;
+    timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 1500);
+  };
   
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragStop);
+
   carousel.addEventListener("scroll", infiniteScroll);
+  autoPlay();
 });
