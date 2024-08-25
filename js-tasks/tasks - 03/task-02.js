cf40192687c96e0c9dae4865a24afba42e0adcb6
Ivan Butryn <ivanbutryn@MacBook-Air-Ivan.local>
2024-08-26 01:16:59 +0300

    Added first and second tasks from codewars part 3

---
+++ js-tasks/tasks - 03/task-02.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+function circleCircumference(circle) {
+    return parseFloat((2 * Math.PI * circle.radius).toFixed(6));
+  }
+  
