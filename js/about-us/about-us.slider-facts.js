ec6989ea066ee1e5efbe379dfa50bbac7dfc024b
puffrogo <126597239+chilliNazar@users.noreply.github.com>
2024-08-29 14:09:29 +0000

    fixed js file with carosuile

--- js/about-us/about-us.slider-facts.js
+++ js/about-us/about-us.slider-facts.js
@@ -1,76 +1,44 @@
-document.body.addEventListener("htmx:afterSettle", (event) => {
-    let isDragging = false;
-    let startX;
-    let startScrollLeft;
-    let timeoutId;
-  
-    const wrapper = document.querySelector(".slider__facts");
-    const carousel = document.querySelector(".slider__facts");
-    const arrowBtns = document.querySelectorAll(".slider__button__left, .slider__button__right");
-    const firstCardWidth = carousel.querySelector("div").offsetWidth;
-    const carouselChildrens = [...carousel.querySelectorAll("div")];
-  
-    let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-  
-    carouselChildrens
-      .slice(-cardPerView)
-      .reverse()
-      .forEach((card) => {
-        carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-      });
-  
-    carouselChildrens
-      .slice(0, cardPerView)
-      .forEach((card) => {
-        carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-      });
-  
-    arrowBtns.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.classList.contains("slider__button__left") 
-          ? -firstCardWidth 
-          : firstCardWidth;
-        infiniteScroll();
-      });
-    });
-  
-    const dragStart = (e) => {
-      isDragging = true;
-      carousel.classList.add("dragging");
-      startX = e.pageX;
-      startScrollLeft = carousel.scrollLeft;
-    };
-  
-    const dragStop = () => {
-      isDragging = false;
-      carousel.classList.remove("dragging");
-    };
-  
-    const dragging = (e) => {
-      if (!isDragging) return;
-      carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-    };
-  
-    const infiniteScroll = () => {
-      if (carousel.scrollLeft === 0) {
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-      } else if (Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.scrollLeft = carousel.offsetWidth;
-      }
-      clearTimeout(timeoutId);
-      if (!wrapper.matches(":hover")) autoPlay();
-    };
-  
-    const autoPlay = () => {
-      if (window.innerWidth < 800) return;
-      timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 1500);
-    };
-  
-    carousel.addEventListener("mousedown", dragStart);
-    carousel.addEventListener("mousemove", dragging);
-    document.addEventListener("mouseup", dragStop);
-  
-    carousel.addEventListener("scroll", infiniteScroll);
-    autoPlay();
-  });
-  
+const slides = [
+  '<div class="slide"><p class="fact__year">2016</p><span class="seperator__slide__2016">-</span><p class="fact__text">The name was changed to EchoWave by our team.</p></div>',
+  '<div class="slide"><p class="fact__year">2020</p><span class="seperator__slide__2020">—</span><p class="fact__text">We gained popularity due to our podcasts during the pandemic.</p></div>',
+  '<div class="slide"><p class="fact__year">2022</p><span class="seperator__slide__2022">—</span><p class="fact__text">Our fantastic team won the Vest NY Radio award.</p></div>',
+  '<div class="slide"><p class="fact__year">2005</p><span class="seperator__slide__2005">—</span><p class="fact__text">Jack Richardson started his own, small radio studio.</p></div>',
+  '<div class="slide"><p class="fact__year">2006</p><span class="seperator__slide__2006">—</span><p class="fact__text">The radio got the TuWave name and started its promo.</p></div>',
+];
+
+let currentSlideIdx = 0;
+
+function renderSlide() {
+  const slideContainer = document.querySelector(".slides-container");
+  slideContainer.innerHTML = slides[currentSlideIdx];
+
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    const secondSlideIdx = (currentSlideIdx + 1) % slides.length;
+    slideContainer.innerHTML += slides[secondSlideIdx];
+
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+      const thirdSlideIdx = (secondSlideIdx + 1) % slides.length;
+      slideContainer.innerHTML += slides[thirdSlideIdx];
+    }
+  }
+}
+
+function nextSlide() {
+  currentSlideIdx = (currentSlideIdx + 1) % slides.length;
+  renderSlide();
+}
+
+function prevSlide() {
+  currentSlideIdx = (currentSlideIdx - 1 + slides.length) % slides.length;
+  renderSlide();
+}
+
+renderSlide();
+
+const btnNext = document.querySelector(".slider__button__right");
+btnNext.addEventListener("click", nextSlide);
+
+const btnPrev = document.querySelector(".slider__button__left");
+btnPrev.addEventListener("click", prevSlide);
+
+window.addEventListener("resize", renderSlide);
