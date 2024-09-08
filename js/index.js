c7a9f47f0370a431568080aba37afddd4ae905ca
puffrogo <126597239+chilliNazar@users.noreply.github.com>
2024-09-08 22:18:02 +0000

    fixed a problem logic html carosuile

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ function init() {
     import('./global/global.header.burgermenu.js');
     import('./index/index.slider-albums.js');
     import('./index/index.subscribe-section.js');
-    import('./about-us/about-us.slider-facts.js');
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
