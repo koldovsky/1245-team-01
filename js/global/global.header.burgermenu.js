934da7695ca91014739d87ae07094dbfb311297c
AndrewTT12 <andriy551071@gmail.com>
2024-09-08 18:37:45 +0300

    Fixed Header + about-us page

--- js/global/global.header.burgermenu.js
+++ js/global/global.header.burgermenu.js
@@ -1,18 +1,15 @@
-document.addEventListener("htmx:load", function () {
-    const burgerToggle = document.getElementById("burger-menu-toggle");
-    const burgerMenu = document.getElementById("burger-menu");
-    const burgerClose = document.getElementById("burger-menu-close");
-  
-    if (burgerToggle) {
-      burgerToggle.addEventListener("click", function () {
-        burgerMenu.classList.toggle("header__menu--open");
-      });
-    }
-  
-    if (burgerClose) {
-      burgerClose.addEventListener("click", function () {
-        burgerMenu.classList.remove("header__menu--open");
-      });
-    }
-  });
+const burgerToggle = document.getElementById("burger-menu-toggle");
+const burgerMenu = document.getElementById("burger-menu");
+const burgerClose = document.getElementById("burger-menu-close");
+  if (burgerToggle) {
+    burgerToggle.addEventListener("click", function () {
+      burgerMenu.classList.toggle("header__menu--open");
+    });
+  }
+
+  if (burgerClose) {
+    burgerClose.addEventListener("click", function () {
+      burgerMenu.classList.remove("header__menu--open");
+    });
+  }
   
